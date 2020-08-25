a0aaf5b7def0696d3edbfd906ccdd95247486507
Trever O <toveson9522@gmail.com>
2020-08-25 17:40:35 -0600

    added .js with pseudo code (I saved this time)

---
+++ weather.js
@@ -0,0 +1,21 @@
+// I need to store user city input
+    // in local storage
+
+// I need to use the users city to pull info from OpenWeather API (https://openweathermap.org/api)
+    // I need to get current: 
+        // City, Date and Weather icon
+        // temp
+        // humidity
+        // wind speed
+        // uv inex (need a color for favorable, moderate, or severe to go with it! )
+    // I also need to get 5 day forcast
+        // date
+        // weather icon
+        // temp
+        // humidity
+        // uv index (This would be extra, Possibly change the background color of the card for the uv index of that day)
+
+// I need to store User input to local storage
+    // I will use this to create dynamic buttons with the city name
+    // these will be bellow the seach 
+    // when they are clickeed they load the weather infor for that city
