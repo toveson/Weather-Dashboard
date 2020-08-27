6853c949d9d759036f3c10ac94078ec43d6bb28a
Trever O <toveson9522@gmail.com>
2020-08-26 18:49:55 -0600

    a push to test

--- weather.js
+++ weather.js
@@ -1,8 +1,37 @@
-// I need to store user city input
-    // in local storage
+// golbal varibles
+
 
 // I need to use the users city to pull info from OpenWeather API (https://openweathermap.org/api)
-    // I need to get current: 
+
+let APIKey = '{54152bc89cd7a8d06acb57627e96a4df}';
+let cityName = 'Springville'
+let queryURL = 'api.openweathermap.org/data/2.5/weather?q={' + cityName + '}&appid=' + APIKey
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    // Create CODE HERE to Log the queryURL
+    console.log(queryURL);
+    // Create CODE HERE to log the resulting object
+    console.log(response)
+  });
+
+
+
+// stuff to store from ajax
+    // storing the searched city
+    // storing the date
+    // storing weather icon
+    // storing the teperature
+    // Storing humidity
+    // Storing wind speed
+    // Storing uv index
+
+
+
+// I need to get current: 
         // City, Date and Weather icon
         // temp
         // humidity
